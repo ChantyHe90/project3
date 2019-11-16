acbd2c9540d3cc581455951d918be835b8bcf411
Chanty He <chanty.he90@googlemail.com>
2019-11-16 14:15:10 +0100

    testing routes to implement in the wireframe

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 class Signup extends React.Component {
-
   state = {
-    username: '',
-    password: '',
-  }
+    username: "",
+    password: ""
+  };
 
-  submitHandler = (event) => {
-    event.preventDefault()
-    axios.post('/api/auth/signup', this.state).then((response) => {
-      this.props.updateUser(response.data)
-      this.props.history.push('/profile')
-    }).catch(() => { })
-  }
+  submitHandler = event => {
+    event.preventDefault();
+    axios
+      .post("/api/auth/signup", this.state)
+      .then(response => {
+        this.props.updateUser(response.data);
+        this.props.history.push("/profile");
+      })
+      .catch(() => {});
+  };
 
-  changeHandler = (event) => {
+  changeHandler = event => {
     this.setState({
       [event.target.name]: event.target.value
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div>
         SIGNUP:
         <form onSubmit={this.submitHandler}>
-          <input name="username" onChange={this.changeHandler} value={this.state.username} type="text" placeholder="username"></input>
+          <input
+            name="username"
+            onChange={this.changeHandler}
+            value={this.state.username}
+            type="text"
+            placeholder="username"
+          ></input>
           <br></br>
-          <input name="password" onChange={this.changeHandler} value={this.state.password} type="text" placeholder="password"></input>
+          <input
+            name="password"
+            onChange={this.changeHandler}
+            value={this.state.password}
+            type="text"
+            placeholder="password"
+          ></input>
           <button type="submit">Submit</button>
         </form>
       </div>
     );
   }
-
 }
 
 export default Signup;
