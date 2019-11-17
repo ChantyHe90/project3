b5815d192ae0b0bd2887f1f2cb3e5e9a4f6932bd
Victor Thoma <victor@mrundmrsthoma.de>
2019-11-17 11:38:00 +0100

    added rest of food list

--- public/data/foodList.js
+++ public/data/foodList.js
@@ -10,6 +10,247 @@
 // glass_cale
 
 let foods = [
+  //fat
+  {
+    name: "butter",
+    img: "",
+    group: "fat",
+    emission: 0.92,
+    car: 6
+  },
+  {
+    name: "margarine",
+    img: "",
+    group: "fat",
+    emission: 0.34,
+    car: 2
+  },
+  {
+    name: "half_fat_magarine",
+    img: "",
+    group: "fat",
+    emission: 0.11,
+    car: 0
+  },
+  {
+    name: "olive_oil",
+    img: "",
+    group: "fat",
+    emission: 0.31,
+    car: 2
+  },
+  {
+    name: "palm_oil",
+    img: "",
+    group: "fat",
+    emission: 0.14,
+    car: 1
+  },
+  {
+    name: "plant_cream",
+    img: "",
+    group: "fat",
+    emission: 0.2,
+    car: 1
+  },
+  {
+    name: "rape_oil",
+    img: "",
+    group: "fat",
+    emission: 0.27,
+    car: 1
+  },
+  {
+    name: "sunflower_oil",
+    img: "",
+    group: "fat",
+    emission: 0.22,
+    car: 1
+  },
+  {
+    name: "vegan_spread",
+    img: "",
+    group: "fat",
+    emission: 0.17,
+    car: 1
+  },
+  {
+    name: "full_fat_margarine",
+    img: "",
+    group: "fat",
+    emission: 0.18,
+    car: 1
+  },
+  //fish
+  {
+    name: "fish",
+    suffix: "fresh",
+    img: "",
+    group: "fish",
+    emission: 0.63,
+    car: 4
+  },
+  {
+    name: "fish",
+    suffix: "frozen",
+    img: "",
+    group: "fish",
+    emission: 0.41,
+    car: 2
+  },
+  {
+    name: "trout",
+    img: "",
+    group: "fish",
+    emission: 0.63,
+    car: 4
+  },
+  {
+    name: "prawns",
+    img: "",
+    group: "fish",
+    emission: 1.26,
+    car: 9
+  },
+  {
+    name: "herring",
+    img: "",
+    group: "fish",
+    emission: 0.63,
+    car: 4
+  },
+  {
+    name: "cod",
+    img: "",
+    group: "fish",
+    emission: 0.63,
+    car: 4
+  },
+  {
+    name: "salmon",
+    img: "",
+    group: "fish",
+    emission: 0.63,
+    car: 4
+  },
+  {
+    name: "mackerel",
+    img: "",
+    group: "fish",
+    emission: 0.63,
+    car: 4
+  },
+  {
+    name: "mussel",
+    img: "",
+    group: "fish",
+    emission: 0.63,
+    car: 4
+  },
+  {
+    name: "rosefish",
+    img: "",
+    group: "fish",
+    emission: 0.63,
+    car: 4
+  },
+  //eggs
+  {
+    name: "eggs",
+    img: "",
+    group: "eggs",
+    emission: 0.2,
+    car: 1
+  },
+  //meat
+  {
+    name: "meatballs",
+    img: "",
+    group: "meat",
+    emission: 0.26,
+    car: 1
+  },
+  {
+    name: "minced_meat",
+    suffix: "mixed",
+    img: "",
+    group: "meat",
+    emission: 0.56,
+    car: 4
+  },
+  {
+    name: "hamburger",
+    suffix: "frozen",
+    img: "",
+    group: "meat",
+    emission: 0.81,
+    car: 5
+  },
+  {
+    name: "chicken",
+    suffix: "frozen",
+    img: "",
+    group: "meat",
+    emission: 0.39,
+    car: 2
+  },
+  {
+    name: "chicken",
+    suffix: "fresh",
+    img: "",
+    group: "meat",
+    emission: 0.37,
+    car: 2
+  },
+  {
+    name: "lamb",
+    img: "",
+    group: "meat",
+    emission: 0.77,
+    car: 5
+  },
+  {
+    name: "turkey",
+    img: "",
+    group: "meat",
+    emission: 0.42,
+    car: 3
+  },
+  {
+    name: "beef",
+    img: "",
+    group: "meat",
+    emission: 1.23,
+    car: 8
+  },
+  {
+    name: "pork",
+    img: "",
+    group: "meat",
+    emission: 0.42,
+    car: 3
+  },
+  {
+    name: "venison",
+    img: "",
+    group: "meat",
+    emission: 1.05,
+    car: 7
+  },
+  {
+    name: "sausage",
+    img: "",
+    group: "meat",
+    emission: 0.36,
+    car: 2
+  },
+  {
+    name: "hotdog",
+    img: "",
+    group: "meat",
+    emission: 0.38,
+    car: 2
+  },
   // veggies
   {
     name: "eggplant",
@@ -464,6 +705,407 @@ let foods = [
     img: "",
     group: "seed",
     emission: 0.1,
+    car: 0
+  },
+  // dairy
+  {
+    name: "buttermilk",
+    img: "",
+    group: "dairy",
+    emission: 0.12,
+    car: 0
+  },
+  {
+    name: "creme_fraiche",
+    img: "",
+    group: "dairy",
+    emission: 0.42,
+    car: 3
+  },
+  {
+    name: "feta_cheese",
+    img: "",
+    group: "dairy",
+    emission: 0.65,
+    car: 4
+  },
+  {
+    name: "cream_cheese",
+    img: "",
+    group: "dairy",
+    emission: 0.57,
+    car: 4
+  },
+  {
+    name: "long_life_milk",
+    img: "",
+    group: "dairy",
+    emission: 0.14,
+    car: 1
+  },
+  {
+    name: "yogurt",
+    img: "",
+    group: "dairy",
+    emission: 0.24,
+    car: 1
+  },
+  {
+    name: "cooking_creme",
+    img: "",
+    group: "dairy",
+    emission: 0.09,
+    car: 0
+  },
+  {
+    name: "cheese",
+    img: "",
+    group: "dairy",
+    emission: 0.58,
+    car: 4
+  },
+  {
+    name: "lowfat_quark",
+    img: "",
+    group: "dairy",
+    emission: 0.25,
+    car: 1
+  },
+  {
+    name: "milk",
+    img: "",
+    group: "dairy",
+    emission: 0.14,
+    car: 1
+  },
+  {
+    name: "mozzarella",
+    img: "",
+    group: "dairy",
+    emission: 0.43,
+    car: 3
+  },
+  {
+    name: "quark",
+    img: "",
+    group: "dairy",
+    emission: 0.34,
+    car: 2
+  },
+  {
+    name: "cream",
+    img: "",
+    group: "dairy",
+    emission: 0.43,
+    car: 3
+  },
+  {
+    name: "cream_alternative",
+    img: "",
+    group: "dairy",
+    emission: 0.1,
+    car: 0
+  },
+  {
+    name: "lowfat_cream",
+    img: "",
+    group: "dairy",
+    emission: 0.3,
+    car: 2
+  },
+  {
+    name: "sour_cream",
+    img: "",
+    group: "dairy",
+    emission: 0.37,
+    car: 2
+  },
+  // fruits
+  {
+    name: "can_ananas",
+    img: "",
+    group: "fruits",
+    emission: 0.17,
+    car: 1
+  },
+  {
+    name: "ananas",
+    img: "",
+    group: "fruits",
+    emission: 0.07,
+    car: 0
+  },
+  {
+    name: "apple",
+    img: "",
+    group: "fruits",
+    emission: 0.03,
+    car: 0
+  },
+  {
+    name: "can_apricot",
+    img: "",
+    group: "fruits",
+    emission: 0.17,
+    car: 1
+  },
+  {
+    name: "apricot",
+    img: "",
+    group: "fruits",
+    emission: 0.05,
+    car: 1
+  },
+  {
+    name: "avocado",
+    img: "",
+    group: "fruits",
+    emission: 0.05,
+    car: 0
+  },
+  {
+    name: "banana",
+    img: "",
+    group: "fruits",
+    emission: 0.06,
+    car: 0
+  },
+  {
+    name: "pear",
+    img: "",
+    group: "fruits",
+    emission: 0.03,
+    car: 0
+  },
+  {
+    name: "blackberry",
+    img: "",
+    group: "fruits",
+    emission: 0.07,
+    car: 0
+  },
+  {
+    name: "strawberry",
+    suffix: "frozen",
+    img: "",
+    group: "fruits",
+    emission: 0.07,
+    car: 0
+  },
+  {
+    name: "strawberry",
+    suffix: "fresh",
+    img: "",
+    group: "fruits",
+    emission: 0.03,
+    car: 0
+  },
+  {
+    name: "figs",
+    img: "",
+    group: "fruits",
+    emission: 0.09,
+    car: 0
+  },
+  {
+    name: "pomegranate",
+    img: "",
+    group: "fruits",
+    emission: 0.04,
+    car: 0
+  },
+  {
+    name: "blueberry",
+    suffix: "frozen",
+    img: "",
+    group: "fruits",
+    emission: 0.06,
+    car: 0
+  },
+  {
+    name: "blueberry",
+    suffix: "fresh",
+    img: "",
+    group: "fruits",
+    emission: 0.05,
+    car: 0
+  },
+  {
+    name: "raspberry",
+    suffix: "frozen",
+    img: "",
+    group: "fruits",
+    emission: 0.07,
+    car: 0
+  },
+  {
+    name: "raspberry",
+    suffix: "fresh",
+    img: "",
+    group: "fruits",
+    emission: 0.12,
+    car: 0
+  },
+  {
+    name: "cherry",
+    img: "",
+    group: "fruits",
+    emission: 0.02,
+    car: 0
+  },
+  {
+    name: "kiwi",
+    img: "",
+    group: "fruits",
+    emission: 0.07,
+    car: 0
+  },
+  {
+    name: "mango",
+    img: "",
+    group: "fruits",
+    emission: 0.17,
+    car: 1
+  },
+  {
+    name: "passion_fruit",
+    img: "",
+    group: "fruits",
+    emission: 0.23,
+    car: 1
+  },
+  {
+    name: "nectarine",
+    img: "",
+    group: "fruits",
+    emission: 0.03,
+    car: 0
+  },
+  {
+    name: "olive",
+    img: "",
+    group: "fruits",
+    emission: 0.15,
+    car: 1
+  },
+  {
+    name: "orange",
+    img: "",
+    group: "fruits",
+    emission: 0.04,
+    car: 0
+  },
+  {
+    name: "peach",
+    img: "",
+    group: "fruits",
+    emission: 0.03,
+    car: 0
+  },
+  {
+    name: "can_peach",
+    img: "",
+    group: "fruits",
+    emission: 0.15,
+    car: 1
+  },
+  {
+    name: "plum",
+    img: "",
+    group: "fruits",
+    emission: 0.02,
+    car: 0
+  },
+  {
+    name: "raisin",
+    img: "",
+    group: "fruits",
+    emission: 0.09,
+    car: 0
+  },
+  {
+    name: "gooseberry",
+    img: "",
+    group: "fruits",
+    emission: 0.07,
+    car: 0
+  },
+  {
+    name: "watermelon",
+    img: "",
+    group: "fruits",
+    emission: 0.04,
+    car: 0
+  },
+  {
+    name: "grapes",
+    img: "",
+    group: "fruits",
+    emission: 0.04,
+    car: 0
+  },
+  {
+    name: "lemon",
+    img: "",
+    group: "fruits",
+    emission: 0.04,
+    car: 0
+  },
+  //spices
+  {
+    name: "vegetable_stock",
+    img: "",
+    group: "spices",
+    emission: 0.15,
+    car: 1
+  },
+  {
+    name: "honey",
+    img: "",
+    group: "spices",
+    emission: 0.19,
+    car: 1
+  },
+  {
+    name: "chicken_broth",
+    img: "",
+    group: "spices",
+    emission: 0.15,
+    car: 1
+  },
+  {
+    name: "coconut_milk",
+    img: "",
+    group: "spices",
+    emission: 0.05,
+    car: 0
+  },
+  {
+    name: "powdered_sugar",
+    img: "",
+    group: "spices",
+    emission: 0.05,
+    car: 0
+  },
+  {
+    name: "beef_broth",
+    img: "",
+    group: "spices",
+    emission: 0.18,
+    car: 1
+  },
+  {
+    name: "soy_milk",
+    img: "",
+    group: "spices",
+    emission: 0.07,
+    car: 0
+  },
+  {
+    name: "sugar",
+    img: "",
+    group: "spices",
+    emission: 0.06,
     car: 0
   }
 ];
