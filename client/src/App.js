1412729408f8ff7c892493ee77a4fc5eea556817
Victor Thoma <victor@mrundmrsthoma.de>
2019-11-19 15:05:30 +0100

    last food co2 values added and minimum code for barcode scan incl. quagga package

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,12 @@ import Signup from "./components/Signup";
 import { Redirect, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import Quagga from "quagga";
+
 class App extends React.Component {
   state = {
-    loggedInUser: this.props.user
+    loggedInUser: this.props.user,
+    detected: []
   };
 
   updateUserHandler = userObj => {
@@ -14,6 +17,55 @@ class App extends React.Component {
       loggedInUser: userObj
     });
   };
+
+  componentDidMount() {
+    Quagga.init(
+      {
+        frequency: 5,
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          area: {
+            // defines rectangle of the detection/localization area
+            top: "20%", // top offset
+            right: "20%", // right offset
+            left: "20%", // left offset
+            bottom: "20%" // bottom offset
+          },
+          target: document.querySelector("#yourElement") // Or '#yourElement' (optional),
+        },
+        decoder: {
+          readers: [
+            "code_128_reader",
+            "ean_reader",
+            "ean_8_reader",
+            "code_39_reader"
+          ],
+          debug: {
+            drawBoundingBox: true,
+            showFrequency: true,
+            drawScanline: true,
+            showPattern: true
+          }
+        }
+      },
+      function(err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        Quagga.start();
+      }
+    );
+    Quagga.onProcessed(data => {
+      console.log("processed", data);
+    });
+    Quagga.onDetected(data => {
+      console.log("detected", data);
+      console.log(data.codeResult);
+      Quagga.offProcessed();
+    });
+  }
 
   render() {
     return (
@@ -53,6 +105,7 @@ class App extends React.Component {
             }}
           ></Route>
         </Switch>
+        <div id="yourElement"></div>;
       </div>
     );
   }
