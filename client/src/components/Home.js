e63b479651ee83324bcf67e14acc8ce07a650777
Chanty He <chanty.he90@googlemail.com>
2019-11-21 21:21:36 +0100

    bootstrap

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div>
+      <div>
+        <h3>Welcome</h3>
+        <p>scan</p>
+        <p>enter manually </p>
+      </div>
+    </div>
+  );
+};
+
+export default home;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-
+import { Navbar, Nav, Button, Form, FormControl } from "react-bootstrap";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-export default class Navbar extends Component {
+export default class myNavbar extends Component {
   LogoutHandler = () => {
     axios
       .get("/api/auth/logout")
@@ -18,9 +18,23 @@ export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <Link to="/login">Log in</Link>
-        <Link to="/signup">Sign Up</Link>
-        <button onClick={this.LogoutHandler}>Logout</button>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Link to="/signup">Signup</Link>
+            <Link to="/login">Log in</Link>
+            <Nav Link href="#pricing">
+              Pricing
+            </Nav>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+          <Button variant="outline-info" onClick={this.LogoutHandler}>
+            Logout
+          </Button>
+        </Navbar>
       </div>
     );
   }
